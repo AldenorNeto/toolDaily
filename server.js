03bbeb45d7b626c588cc569cf798e55e0f105828
Aldenor Arruda Gomes Neto <arruda.neto@grendene.com.br>
2024-04-29 22:53:08 -0300

    feat: adicionando server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 5550;
+
+app.use(express.static('.'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ start_server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Users\ar9185\Desktop\EstudosDeCaso\toolDaily\server.js"
+node server.js
