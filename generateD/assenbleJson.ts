61eacfe24ef7c3f743929715084b7518964e81c4
Aldenor Neto <aldenor.code@gmail.com>
2024-07-21 21:47:51 -0300

    feat: tipando aplicação

---
+++ generateD/assenbleJson.ts
@@ -0,0 +1,113 @@
+import fs from "fs";
+import { JSDOM } from "jsdom";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const inputDir = path.join(__dirname, "indices");
+const outputFile = path.join(__dirname, "../showD/agenda.json");
+
+interface Task {
+  title: string;
+  subTitle: string;
+  id: string;
+  periodo: string;
+}
+
+interface SimplifiedHTML {
+  name: string;
+  task: Task[];
+}
+
+function processarHTML(inputFile: string): Promise<SimplifiedHTML> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(inputFile, "utf8", (err, dataHTML) => {
+      if (err) {
+        console.error("Erro ao ler o arquivo:", err);
+        reject(err);
+        return;
+      }
+
+      const dom = new JSDOM(dataHTML);
+      const document = dom.window.document;
+
+      const activitiesContainer = document.querySelector(
+        "#js-activities-container"
+      );
+      if (!activitiesContainer) {
+        reject(new Error("Container de atividades não encontrado"));
+        return;
+      }
+
+      const subElements: NodeListOf<Element> =
+        activitiesContainer.querySelectorAll(".timeline-panel");
+      const elementsArray = Array.from(subElements);
+
+      const filtrosFinalizado: Task[] = elementsArray.map((element) => {
+        const title =
+          element
+            .querySelector("span")
+            ?.textContent?.trim()
+            .split("    ")[0]
+            .replaceAll(/\n/g, "") || "";
+
+        const arrayPeriodos =
+          (Array.from(element.querySelectorAll("small"))[1]?.textContent ?? "")
+            .trim()
+            .split("    ") || [];
+
+        const subTitle =
+          (Array.from(element.querySelectorAll("small"))[0]?.textContent ?? "")
+            .trim()
+            .split("    ")[0]
+            .replace("  ", " ") || "";
+
+        const periodo =
+          arrayPeriodos[arrayPeriodos.length - 1]
+            ?.trim()
+            .replace("Período:", "")
+            .trim() || "";
+
+        return { title, subTitle, id: uuidv4(), periodo };
+      });
+
+      const simplifiedHTML: SimplifiedHTML = {
+        name: path
+          .basename(inputFile, path.extname(inputFile))
+          .split("-")[1]
+          .split("(")[0]
+          .trim(),
+        task: filtrosFinalizado,
+      };
+
+      resolve(simplifiedHTML);
+    });
+  });
+}
+
+fs.readdir(inputDir, async (err, files) => {
+  if (err) {
+    console.error("Erro ao ler o diretório:", err);
+    return;
+  }
+
+  const htmlFiles = files.filter((file) => path.extname(file) === ".html");
+  const objetosArray: SimplifiedHTML[] = [];
+
+  for (const file of htmlFiles) {
+    const inputFile = path.join(inputDir, file);
+    try {
+      const processedData = await processarHTML(inputFile);
+      objetosArray.push(processedData);
+    } catch (error) {
+      console.error(`Erro ao processar o arquivo ${file}:`, error);
+    }
+  }
+
+  fs.writeFile(outputFile, JSON.stringify(objetosArray, null, 2), (err) => {
+    if (err) {
+      console.error("Erro ao salvar o arquivo:", err);
+      return;
+    }
+    console.log("Arquivo processado e salvo com sucesso!");
+  });
+});
--- showD/server.ts
+++ showD/server.ts
@@ -1,46 +1,61 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+import express, { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+
 const app = express();
 const PORT = 5550;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (_, res) => {
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+interface Course {
+  name: string;
+  task: Task[];
+}
+
+type Agenda = Course[];
+
+app.get("/", (_, res: Response) => {
   const htmlContent = `
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Agenda UNOPAR</title>
-    <link
-      rel="icon"
-      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 16 16%22><text y=%2214%22 font-size=%2214%22>🗓️</text></svg>"
-    />
-    <link rel="stylesheet" href="/styles.css">
-  </head>
-  <body>
-    <script src="/script.js"></script>
-  </body>
-</html>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Agenda UNOPAR</title>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 16 16%22><text y=%2214%22 font-size=%2214%22>🗓️</text></svg>"
+      />
+      <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+      <script src="/script.js"></script>
+    </body>
+  </html>
   `;
   res.send(htmlContent);
 });
 
-app.get("/data", (_, res) => {
+app.get("/data", (_, res: Response) => {
   fs.readFile("./showD/agenda.json", "utf8", (err, data) => {
     if (err) {
       console.error("Erro ao ler o arquivo JSON:", err);
       return res.status(500).json({ error: "Erro ao ler o arquivo JSON" });
     }
-    res.json(JSON.parse(data));
+    const agenda: Agenda = JSON.parse(data);
+    res.json(agenda);
   });
 });
 
-app.post("/checked", (req, res) => {
-  const { id, done } = req.body;
+app.post("/checked", (req: Request, res: Response) => {
+  const { id, done } = req.body as { id: number; done: boolean };
 
   fs.readFile("./showD/agenda.json", "utf8", (err, data) => {
     if (err) {
@@ -48,7 +63,7 @@ app.post("/checked", (req, res) => {
       return res.status(500).json({ error: "Erro ao ler o arquivo JSON" });
     }
 
-    let agenda = JSON.parse(data);
+    const agenda: Agenda = JSON.parse(data);
 
     for (let course of agenda) {
       for (let task of course.task) {
